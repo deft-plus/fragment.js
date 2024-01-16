ccf193c2d85c57a222ed1c70439662cb9a069286
Miguel Bogota <miguel.bogota@mercadolibre.com.co>
2024-01-16 17:46:33 -0500

    Adds 'publish' module to automate tag generation

--- cli/mod.ts
+++ cli/mod.ts
@@ -1,10 +1,11 @@
 import { cli } from '@app_deps.ts';
 
 import { dev } from './dev.ts';
+import { VERSION } from '../version.ts';
 
 await new cli.cliffy.command.Command()
   .name('Fragmente TS')
-  .version('0.0.1')
+  .version(VERSION)
   .description('Fragmente TS is a tool to help you create and manage your web components')
   .command('dev', dev)
   .parse(Deno.args);
